a4a93537fadd5967fd1584aba741ad29cfb80a80
damirpolat <dpulatov@uwyo.edu>
2020-06-30 22:05:06 -0600

    created basic bar plot

--- d3-plot/barplot.js
+++ d3-plot/barplot.js
@@ -1,32 +1,40 @@
-// !preview r2d3 data=c(0.3, 0.6, 0.8, 0.95, 0.40, 0.20)
+// !preview r2d3 data=read.csv("data.csv") 
 //
 // r2d3: https://rstudio.github.io/r2d3
 //
+var axisLine = height / 2;
 
-var w = 600;
-var h = 250;
-
+// create x scale
 var xScale = d3.scaleBand()
         .domain(d3.range(data.length))
-        .rangeRound([0, w])
+        .rangeRound([0, width])
         .paddingInner(0.05);
 
+// create y scale
 var yScale = d3.scaleLinear()
-        .domain([0, d3.max(data)])
-        .range([0, h]);
+        .domain([d3.min(data, function(d) { return d.RMSE; }), 
+                  d3.max(data, function(d) { return d.RMSE; })])
+        .range([height, 0]);
 
-
+// adding bars
 svg.selectAll('rect')
   .data(data)
   .enter().append('rect') 
     .attr('width', xScale.bandwidth())
     .attr('height', function(d) {
-      return yScale(d);
+      return Math.abs(yScale(d.RMSE) - yScale(0));
     })
     .attr('y', function(d) { 
-      return h - yScale(d); 
+      if(d.RMSE > 0) {
+        return yScale(d.RMSE);
+      } else {
+        return yScale(0);
+      }      
     })
     .attr('x', function(d, i) {
       return xScale(i);
     })
-    .attr('fill', 'steelblue');
+    .attr('fill', 'steelblue')
+    .attr('opacity', 0.8);
+
+// add axis
--- d3-plot/test.R
+++ d3-plot/test.R
@@ -1,6 +1,21 @@
 # testing r2d3
+library(aslib)
+library(r2d3)
+
 load("../data/id.RData")
 model1 = combined_model
 load("../data/metrics.RData")
 model2 = combined_model
 
+sc = parseASScenario("../data/aslib_scenarios/SAT11-INDU/")
+data = convertToLlamaCVFolds(sc)
+data = fixFeckingPresolve(sc, data)
+
+error1 = calculate_errors(sc, data, model1)
+error2 = calculate_errors(sc, data, model2)
+errors = data.frame(matrix(ncol = 2, nrow = length(error1[, 1])))
+colnames(errors) = c("RMSE", "solver")
+errors$RMSE = error1$RMSE - error2$RMSE
+errors$solver = error1$solver
+
+r2d3(data = errors, script = "./barplot.js")
